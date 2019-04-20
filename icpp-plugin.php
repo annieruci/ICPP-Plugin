4849d8f8b7e823d3518969e90985aa2e344beb2b
annier <annieruci@gmail.com>
2019-04-20 15:47:22 +0200

    nuevo cambio de prueba

--- icpp-plugin.php
+++ icpp-plugin.php
@@ -4,6 +4,7 @@ Plugin Name: ICPP Plugin
 Description: Este Plugin carga un archivo EXEL y crea nuevos contenidos con la informacion!
 Author: Icomppluselec
 */
+//Linea de prueba.
 // Include icpp-functions.php, use require_once to stop the script if mfp-functions.php is not found
 require_once plugin_dir_path(__FILE__) . 'includes/icpp-functions.php';
 require_once plugin_dir_path(__FILE__) . 'includes/Classes/PHPExcel.php';
